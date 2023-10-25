eebf60c2bd611f44c0d072e14a2bf4d6e0110c00
unknown <daviddimas80@gmail.com>
2023-10-25 20:39:42 +0700

    add and get

--- GymInns/Controllers/UsersController.cs
+++ GymInns/Controllers/UsersController.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GymInns.Data;
+using GymInns.Models;
+using GymInns.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymInns.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly MVCDemoDbContext mvcdemoDbContext;
+        public UsersController(MVCDemoDbContext mvcdemoDbContext)
+        {
+            this.mvcdemoDbContext = mvcdemoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+          var users = await  mvcdemoDbContext.Users.ToListAsync();
+            return View(users);
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddVUserViewModel addVUserViewModel)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = addVUserViewModel.Name,
+                Email = addVUserViewModel.Email,
+                Password = addVUserViewModel.Password,
+                Plan = addVUserViewModel.Plan,
+                DateJoin = addVUserViewModel.DateJoin,
+                DateExpired = addVUserViewModel.DateExpired,
+            };
+            await mvcdemoDbContext.Users.AddAsync(user);
+            await mvcdemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
         }
 
 
