debf5d579a47ee0fa5c2af73a93b3fc330d73b60
unknown <daviddimas80@gmail.com>
2023-10-28 17:03:44 +0700

    complete crud

--- GymInns/Controllers/UsersController.cs
+++ GymInns/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using GymInns.Models;
 using GymInns.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 namespace GymInns.Controllers
 {
@@ -45,6 +46,61 @@ namespace GymInns.Controllers
             return RedirectToAction("Add");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+           var user = await mvcdemoDbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
 
-    }
+            if (user != null)
+            {
+                var editModel = new UpdateUserViewModel()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    Password = user.Password,
+                    Plan = user.Plan,
+                    DateJoin = user.DateJoin,
+                    DateExpired = user.DateExpired,
+                };
+                return View(editModel);
+            }
+
+            return RedirectToAction("Index");
+         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateUserViewModel model)
+        {
+            var user = await mvcdemoDbContext.Users.FindAsync(model.Id);
+            
+            if (user != null)
+            {
+                user.Name = model.Name;
+                user.Email = model.Email;
+                user.Plan = model.Plan;
+                user.DateJoin = model.DateJoin;
+                user.DateExpired = model.DateExpired;
+
+               await mvcdemoDbContext.SaveChangesAsync();
+               return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await mvcdemoDbContext.Users.FindAsync(id);
+            if(user != null) 
+            {
+                mvcdemoDbContext.Users.Remove(user);
+                await mvcdemoDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        }
 }
