e8cadd825a7598e7800e373912ccc1d9b0ebe4dd
unknown <daviddimas80@gmail.com>
2023-11-01 12:03:04 +0700

    detail product

---
+++ GymInns/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using GymInns.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymInns.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDBcontext;
+
+        public ProductController(MVCDemoDbContext mvcDemoDBcontext)
+        {
+            this.mvcDemoDBcontext = mvcDemoDBcontext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int Id)
+        {
+            var productDetail = await mvcDemoDBcontext.Products.FirstOrDefaultAsync(x=>x.ID==Id);
+            return View(productDetail);
+        }
+    }
+}
