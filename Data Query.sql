024d6c76dd6cba1fb176e20c6ee3a2937b12c605
unknown <daviddimas80@gmail.com>
2023-11-01 17:21:31 +0700

    navbar fix

--- Data Query.sql
+++ Data Query.sql
@@ -9,4 +9,5 @@ categories varchar(50)
 
 --insert into GymDb.dbo.Products values(1,'Sport Shirt','Shirt designed and worn for sporting pursuits',100000,'/image/shirt.png','Shirt')
 --insert into GymDb.dbo.Products values (2,'Pants','Pants designed and worn for sporting pursuits',50000,'/image/pants.png','Pants')
+--insert into GymDb.dbo.Products values (3,'Shoes Sport 1','Shoes designed and worn for sporting pursuits',50000,'/image/shoes.png','Shoes')
 --select * from GymDb.dbo.Product
